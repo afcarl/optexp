1cdff3d54ef498bc02271d25d1d5bab47d82cab4
Chi Feng <email.chi.feng@gmail.com>
2015-07-31 11:41:23 -0700

    Added MIS flag to Example-1

--- src/Example-1.cpp
+++ src/Example-1.cpp
@@ -15,9 +15,11 @@ int main(int argc, char **argv)
   auto experiment = make_shared<SimpleExperiment>(4, 1, 0.4);
 
   auto estimator = make_shared<ExpectedInformationEstimator>(experiment);
-  estimator->N = 100;  // outer Monte Carlo samples
-  estimator->M1 = 100; // inner Monte Carlo samples
-  estimator->M2 = 100; // inner Monte Carlo samples
+  estimator->N = 500; // outer Monte Carlo samples
+  estimator->M1 = 50; // inner Monte Carlo samples
+  estimator->M2 = 50; // inner Monte Carlo samples
+
+  estimator->useMIS = true; // use Multiple Importance Sampling
 
   VectorXd design = VectorXd::Constant(1, 0.5); // input is 1-dimensional
 
